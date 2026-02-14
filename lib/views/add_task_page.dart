972bdb809e48b2258179ca30c576ba5fee657a3b
joshitha1808 <joshitha0818@gmail.com>
2026-02-14 23:54:39 +0530

    feat: Implement AddTaskPage with UI for creating new tasks

---
+++ lib/views/add_task_page.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+import 'package:planit/views/widgets/category_selector.dart';
+
+class AddTaskPage extends StatefulWidget {
+  const AddTaskPage({super.key});
+
+  @override
+  State<AddTaskPage> createState() => _AddTaskPageState();
+}
+
+class _AddTaskPageState extends State<AddTaskPage> {
+  String selectedOption = "Today";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(automaticallyImplyLeading: false, toolbarHeight: 2),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            IconButton(
+              style: IconButton.styleFrom(
+                padding: EdgeInsets.all(22),
+                side: BorderSide(
+                  width: 2,
+                  color: Theme.of(context).colorScheme.outlineVariant,
+                ),
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.close_rounded, size: 30),
+            ),
+            Text(
+              "New Task",
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                // color: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                IconButton(
+                  style: IconButton.styleFrom(
+                    padding: EdgeInsets.all(22),
+                    side: BorderSide(
+                      width: 2,
+                      color: Theme.of(context).colorScheme.outlineVariant,
+                    ),
+                  ),
+                  onPressed: () {},
+                  icon: Icon(Icons.add, size: 40),
+                ),
+                SizedBox(width: 18),
+
+                ChoiceChip(
+                  label: Text(
+                    "Today",
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                  showCheckmark: false,
+                  selected: selectedOption == "Today",
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(36),
+                  ),
+                  onSelected: (selected) {
+                    setState(() {
+                      selectedOption = "Today";
+                    });
+                  },
+                ),
+                SizedBox(width: 18),
+                ChoiceChip(
+                  label: Text(
+                    "Tomorrow",
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                  showCheckmark: false,
+                  selected: selectedOption == "Tomorrow",
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  onSelected: (selected) {
+                    setState(() {
+                      selectedOption = "Tomorrow";
+                    });
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                IconButton(
+                  style: IconButton.styleFrom(
+                    padding: EdgeInsets.all(22),
+                    side: BorderSide(
+                      width: 2,
+                      color: Theme.of(context).colorScheme.outlineVariant,
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(Icons.add_alarm_outlined, size: 40),
+                ),
+                SizedBox(width: 10),
+
+                IconButton(
+                  style: IconButton.styleFrom(
+                    padding: EdgeInsets.all(22),
+                    side: BorderSide(
+                      width: 2,
+                      color: Theme.of(context).colorScheme.outlineVariant,
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(Icons.notification_add_outlined, size: 40),
+                ),
+              ],
+            ),
+            SizedBox(height: 50),
+            Text(
+              "CATEGORIES",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).colorScheme.onPrimaryFixed,
+              ),
+            ),
+            SizedBox(height: 20),
+            CategorySelector(),
+
+            SizedBox(height: 20),
+            Text(
+              "TITLE",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).colorScheme.onPrimaryFixed,
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "DESCRIPTION",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).colorScheme.onPrimaryFixed,
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(16)),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
