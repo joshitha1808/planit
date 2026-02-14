4523d6b26c55d6433fcb51e7893eaa3f44d06889
joshitha1808 <joshitha0818@gmail.com>
2026-02-15 00:17:34 +0530

    feat: Refactor AddTaskPage layout for improved readability and UI consistency

--- lib/views/add_task_page.dart
+++ lib/views/add_task_page.dart
@@ -16,153 +16,184 @@ class _AddTaskPageState extends State<AddTaskPage> {
       appBar: AppBar(automaticallyImplyLeading: false, toolbarHeight: 2),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            IconButton(
-              style: IconButton.styleFrom(
-                padding: EdgeInsets.all(22),
-                side: BorderSide(
-                  width: 2,
-                  color: Theme.of(context).colorScheme.outlineVariant,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              IconButton(
+                style: IconButton.styleFrom(
+                  padding: EdgeInsets.all(22),
+                  side: BorderSide(
+                    width: 2,
+                    color: Theme.of(context).colorScheme.outlineVariant,
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: Icon(Icons.close_rounded, size: 24),
+              ),
+              Text(
+                "New Task",
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  // color: Theme.of(context).colorScheme.primary,
                 ),
               ),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: Icon(Icons.close_rounded, size: 30),
-            ),
-            Text(
-              "New Task",
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
-                // color: Theme.of(context).colorScheme.primary,
-              ),
-            ),
-            SizedBox(height: 20),
-            Row(
-              children: [
-                IconButton(
-                  style: IconButton.styleFrom(
-                    padding: EdgeInsets.all(22),
-                    side: BorderSide(
-                      width: 2,
-                      color: Theme.of(context).colorScheme.outlineVariant,
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  IconButton(
+                    style: IconButton.styleFrom(
+                      padding: EdgeInsets.all(22),
+                      side: BorderSide(
+                        width: 2,
+                        color: Theme.of(context).colorScheme.outlineVariant,
+                      ),
                     ),
+                    onPressed: () {},
+                    icon: Icon(Icons.add, size: 24),
                   ),
-                  onPressed: () {},
-                  icon: Icon(Icons.add, size: 40),
-                ),
-                SizedBox(width: 18),
+                  SizedBox(width: 18),
 
-                ChoiceChip(
-                  label: Text(
-                    "Today",
-                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ChoiceChip(
+                    label: Text(
+                      "Today",
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    showCheckmark: false,
+                    selected: selectedOption == "Today",
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(36),
+                    ),
+                    onSelected: (selected) {
+                      setState(() {
+                        selectedOption = "Today";
+                      });
+                    },
                   ),
-                  showCheckmark: false,
-                  selected: selectedOption == "Today",
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(36),
+                  SizedBox(width: 18),
+                  ChoiceChip(
+                    label: Text(
+                      "Tomorrow",
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    showCheckmark: false,
+                    selected: selectedOption == "Tomorrow",
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    onSelected: (selected) {
+                      setState(() {
+                        selectedOption = "Tomorrow";
+                      });
+                    },
                   ),
-                  onSelected: (selected) {
-                    setState(() {
-                      selectedOption = "Today";
-                    });
-                  },
+                ],
+              ),
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  IconButton(
+                    style: IconButton.styleFrom(
+                      padding: EdgeInsets.all(22),
+                      side: BorderSide(
+                        width: 2,
+                        color: Theme.of(context).colorScheme.outlineVariant,
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(Icons.add_alarm_outlined, size: 30),
+                  ),
+                  SizedBox(width: 10),
+
+                  IconButton(
+                    style: IconButton.styleFrom(
+                      padding: EdgeInsets.all(22),
+                      side: BorderSide(
+                        width: 2,
+                        color: Theme.of(context).colorScheme.outlineVariant,
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(Icons.notification_add_outlined, size: 30),
+                  ),
+                ],
+              ),
+              SizedBox(height: 50),
+              Text(
+                "CATEGORIES",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.onPrimaryFixed,
                 ),
-                SizedBox(width: 18),
-                ChoiceChip(
-                  label: Text(
-                    "Tomorrow",
-                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                  ),
-                  showCheckmark: false,
-                  selected: selectedOption == "Tomorrow",
-                  shape: RoundedRectangleBorder(
+              ),
+              SizedBox(height: 20),
+              CategorySelector(),
+
+              SizedBox(height: 20),
+              Text(
+                "TITLE",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.onPrimaryFixed,
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(30),
                   ),
-                  onSelected: (selected) {
-                    setState(() {
-                      selectedOption = "Tomorrow";
-                    });
-                  },
                 ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Row(
-              children: [
-                IconButton(
-                  style: IconButton.styleFrom(
-                    padding: EdgeInsets.all(22),
-                    side: BorderSide(
-                      width: 2,
-                      color: Theme.of(context).colorScheme.outlineVariant,
+              ),
+              SizedBox(height: 20),
+
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Description(optional)",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+              SizedBox(height: 40),
+              Center(
+                child: SizedBox(
+                  width:
+                      MediaQuery.of(context).size.width * 0.9, // Make it wide
+                  height: 80,
+
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      foregroundColor: Theme.of(context).colorScheme.onPrimary,
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      "Create",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
                   ),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  icon: Icon(Icons.add_alarm_outlined, size: 40),
-                ),
-                SizedBox(width: 10),
-
-                IconButton(
-                  style: IconButton.styleFrom(
-                    padding: EdgeInsets.all(22),
-                    side: BorderSide(
-                      width: 2,
-                      color: Theme.of(context).colorScheme.outlineVariant,
-                    ),
-                  ),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  icon: Icon(Icons.notification_add_outlined, size: 40),
-                ),
-              ],
-            ),
-            SizedBox(height: 50),
-            Text(
-              "CATEGORIES",
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: Theme.of(context).colorScheme.onPrimaryFixed,
-              ),
-            ),
-            SizedBox(height: 20),
-            CategorySelector(),
-
-            SizedBox(height: 20),
-            Text(
-              "TITLE",
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: Theme.of(context).colorScheme.onPrimaryFixed,
-              ),
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
                 ),
               ),
-            ),
-            SizedBox(height: 20),
-
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: "Description(optional)",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
