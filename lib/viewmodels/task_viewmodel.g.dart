4bc06c8e24e4d53046ffcf67d9be4df494a98c6b
joshitha1808 <joshitha0818@gmail.com>
2026-02-14 12:28:18 +0530

    feat: Add generated TaskViewModel provider for Riverpod integration

---
+++ lib/viewmodels/task_viewmodel.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'task_viewmodel.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$taskViewModelHash() => r'1f59e742d650ee0f2b0d7743a78d6aad706d5023';
+
+/// See also [TaskViewModel].
+@ProviderFor(TaskViewModel)
+final taskViewModelProvider =
+    AutoDisposeNotifierProvider<TaskViewModel, AsyncValue<List<Task>>>.internal(
+      TaskViewModel.new,
+      name: r'taskViewModelProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$taskViewModelHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$TaskViewModel = AutoDisposeNotifier<AsyncValue<List<Task>>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
