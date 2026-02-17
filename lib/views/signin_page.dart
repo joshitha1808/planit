8a119bf64e8f35b1b64a6f69e36c3da0899d9479
joshitha1808 <joshitha0818@gmail.com>
2026-02-17 22:48:56 +0530

    feat: create ConsumerStatefulWidget signin page

---
+++ lib/views/signin_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class SigninPage extends ConsumerStatefulWidget {
+  const SigninPage({super.key});
+
+  @override
+  ConsumerState<SigninPage> createState() => _SigninPageState();
+}
+
+class _SigninPageState extends ConsumerState<SigninPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
