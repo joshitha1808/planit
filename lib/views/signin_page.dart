12fed94b63b5c2224cc38885ebaa16988f4af19c
joshitha1808 <joshitha0818@gmail.com>
2026-02-17 22:54:53 +0530

    fix: handle auth error with snackbar

--- lib/views/signin_page.dart
+++ lib/views/signin_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:planit/viewmodels/auth_viewmodel.dart';
 
 class SigninPage extends ConsumerStatefulWidget {
   const SigninPage({super.key});
@@ -11,9 +12,29 @@ class SigninPage extends ConsumerStatefulWidget {
 class _SigninPageState extends ConsumerState<SigninPage> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
+    final authState = ref.watch(authViewModelProvider);
+    ref.listen(authViewModelProvider, (_, next) {
+      next?.when(
+        data: (data) {
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => const HomePage()),
+            (_) => false,
+          );
+        },
+
+        error: (error, st) {
+          showSnackBar(context, error.toString());
+        },
+
+        loading: () {},
+      );
+    });
+
     return Container();
   }
 }
