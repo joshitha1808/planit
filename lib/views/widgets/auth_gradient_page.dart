5a5a2a1c4905fc02974d38a64d0572899e3df7fd
joshitha1808 <joshitha0818@gmail.com>
2026-02-17 23:49:58 +0530

    feat: add auth gradient page

---
+++ lib/views/widgets/auth_gradient_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class AuthGradientPage extends StatelessWidget {
+  final String buttonText;
+  final VoidCallback onTap;
+
+  const AuthGradientPage({
+    super.key,
+    required this.buttonText,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.red, Colors.orange],
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+        ),
+        borderRadius: BorderRadius.all(Radius.circular(7)),
+      ),
+      child: ElevatedButton(
+        onPressed: onTap,
+        style: ElevatedButton.styleFrom(
+          fixedSize: const Size(double.infinity, 55),
+          backgroundColor: Colors.transparent,
+          shadowColor: Colors.transparent,
+        ),
+        child: Text(
+          buttonText,
+          style: const TextStyle(
+            fontSize: 17,
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
+  }
+}
