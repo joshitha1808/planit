20a52ac4d4f3d484667db5c66925f96d77ff0b8f
joshitha1808 <joshitha0818@gmail.com>
2026-02-17 22:25:07 +0530

    feat : create auth remote repository file

---
+++ lib/repository/auth_remote_repository.dart
@@ -0,0 +1,67 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:planit/core/error/failure.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+part 'auth_remote_repository.g.dart';
+
+@riverpod
+AuthRemoteRepository authRemoteRepository(AuthRemoteRepositoryRef ref) {
+  return AuthRemoteRepository(Supabase.instance.client);
+}
+
+class AuthRemoteRepository {
+  final SupabaseClient supabaseClient;
+
+  AuthRemoteRepository(this.supabaseClient);
+
+  Future<Either<Failure, User>> signin({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final response = await supabaseClient.auth.signInWithPassword(
+        email: email,
+        password: password,
+      );
+
+      final user = response.user;
+
+      if (user == null) {
+        return left(Failure('Login failed. No user found.'));
+      }
+
+      return right(user);
+    } on AuthException catch (e) {
+      return left(Failure(e.message));
+    } catch (e) {
+      return left(Failure(e.toString()));
+    }
+  }
+
+  Future<Either<Failure, User>> signup({
+    required String userName,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final response = await supabaseClient.auth.signUp(
+        data: {"username": userName},
+        email: email,
+        password: password,
+      );
+
+      final user = response.user;
+
+      if (user == null) {
+        return left(Failure('Signup failed. Unable to create user.'));
+      }
+
+      return right(user);
+    } on AuthException catch (e) {
+      return left(Failure(e.message));
+    } catch (e) {
+      return left(Failure(e.toString()));
+    }
+  }
+}
